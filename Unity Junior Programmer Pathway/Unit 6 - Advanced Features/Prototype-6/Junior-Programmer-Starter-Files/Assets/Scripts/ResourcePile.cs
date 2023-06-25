1ed9348961ad89c13a60c2bb2c3fb641645f2fb5
Fartomy <furkii_tekdemir@hotmail.com>
2023-06-25 22:49:50 +0300

    prototype-6 project files added and old proto6.unitypackage file deprecated

---
+++ Unity Junior Programmer Pathway/Unit 6 - Advanced Features/Prototype-6/Junior-Programmer-Starter-Files/Assets/Scripts/ResourcePile.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A subclass of Building that produce resource at a constant rate.
+/// </summary>
+public class ResourcePile : Building
+{
+    public ResourceItem Item;
+
+    public float ProductionSpeed;
+    private float m_ProductionSpeed = 0.5f;
+
+    private float m_CurrentProduction
+    {
+        get { return ProductionSpeed; }
+        set
+        {
+            if (value < 0.0f)
+                Debug.LogError("Production speed cannot be negative");
+            else
+                m_ProductionSpeed = value;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_CurrentProduction > 1.0f)
+        {
+            int amountToAdd = Mathf.FloorToInt(m_CurrentProduction);
+            int leftOver = AddItem(Item.Id, amountToAdd);
+
+            m_CurrentProduction = m_CurrentProduction - amountToAdd + leftOver;
+        }
+        
+        if (m_CurrentProduction < 1.0f)
+        {
+            m_CurrentProduction += m_ProductionSpeed * Time.deltaTime;
+        }
+    }
+
+    public override string GetData()
+    {
+        return $"Producing at the speed of {m_ProductionSpeed}/s";
+        
+    }
+    
+    
+}
